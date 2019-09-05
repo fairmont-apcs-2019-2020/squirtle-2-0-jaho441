cd7c88b8484fb2a3c09200924c9ca99a0187363e
megannaho <megannaho@10.2.3.159>
2019-09-05 08:47:18 -0700

    I have moved the location of the squirtle so it hits and goes over the flower.

--- Squirtle/src/SquirtleBoard.java
+++ Squirtle/src/SquirtleBoard.java
@@ -19,12 +19,12 @@ public class SquirtleBoard extends Board {
 		
 		// Create a SquirtleThing at 0, 0
 	 	SquirtleThing squirtle = new SquirtleThing();
-	 	squirtle.setLocation(0, 0);
+	 	squirtle.setLocation(4, 0);
 	 	add(squirtle);
 	 	
 	 	// Create a BulbasaurThing at 0, 4
 	 	BulbasaurThing bulbasaur = new BulbasaurThing();
-	 	bulbasaur.setLocation(4, 0);
+	 	bulbasaur.setLocation(4, 2);
 	 	add(bulbasaur);
 	 	
 	}
